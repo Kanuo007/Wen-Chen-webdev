7371b61544e46f1b044ed2d27a43c2b426f93e66
Kanuo007 <chenwenjiayou@gmail.com>
2016-10-02 18:48:16 -0700

    hw2

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use('/public/client/profile.html');
-// app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
