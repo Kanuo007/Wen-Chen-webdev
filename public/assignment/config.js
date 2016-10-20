b6d7071388860b86691a2cb2f175d7423148546f
Kanuo007 <chenwenjiayou@gmail.com>
2016-10-19 23:46:32 -0700

    hw3

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -11,42 +11,42 @@
         $routeProvider
             /* html template talk to controller by model*/
             .when("/login", {
-                templateUrl: "/views/user/login.view.client.html",
+                templateUrl: "views/user/login.view.client.html",
                 controller: "LoginController",
                 controllerAs: "model"
             })
             .when("/register", {
-                templateUrl: "/views/user/register.view.client.html",
+                templateUrl: "/webdev/public/assignment/views/user/register.view.client.html",
                 controller: "RegisterController",
                 controllerAs: "model"
             })
             .when("/user/:uid", {
-                templateUrl: "/views/user/profile.view.client.html",
+                templateUrl: "/webdev/public/assignment/views/user/profile.view.client.html",
                 controller: "ProfileController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website", {
-                templateUrl: "/views/website/website-list.view.client.html",
+                templateUrl: "/webdev/public/assignment/views/website/website-list.view.client.html",
                 controller: "WebsiteListController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/new", {
-                templateUrl: "/views/website/website-new.view.client.html",
+                templateUrl: "/webdev/public/assignment/views/website/website-new.view.client.html",
                 controller: "NewWebsiteController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid", {
-                templateUrl: "/views/website/website-edit.view.client.html",
+                templateUrl: "/webdev/public/assignment/views/website/website-edit.view.client.html",
                 controller: "EditWebsiteController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page", {
-                templateUrl: "/views/page/page-list.view.client.html",
+                templateUrl: "/webdev/public/assignment/views/page/page-list.view.client.html",
                 controller: "PageListController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/new", {
-                templateUrl: "/views/page/page-new.view.client.html",
+                templateUrl: "/webdev/public/assignment/views/page/page-new.view.client.html",
                 controller: "NewPageController",
                 controllerAs: "model"
             })
